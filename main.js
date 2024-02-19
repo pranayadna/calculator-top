fabcd6f30eb387d6dbc66b603649a274568bbbbf
pranayadna <pranayadnaap96@gmail.com>
2024-02-19 16:59:24 +0700

    Add an add, subtract, multiply, and divide functions

---
+++ main.js
@@ -0,0 +1,19 @@
+// add
+const add = function(num1, num2) {
+	return num1 + num2;
+};
+
+// subtract
+const subtract = function(num1, num2) {
+	return num1 - num2;
+};
+
+// multiply
+const multiply = function(num1, num2) {
+    return num1 * num2;
+};
+
+// divide
+const divide = function(num1, num2) {
+    return num1 / num2;
+};
